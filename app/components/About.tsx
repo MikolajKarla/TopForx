cd0a319bbadeec2fe424c4c5e798de2b2d16525d
Mikolaj <mikolajkarla@gmail.com>
2023-10-03 17:30:41 +0200

    Ended Slider and starting opinion section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,11 +1,80 @@
-import React from 'react'
+"use client";
+import React, { use, useEffect, useState } from "react";
+
+import {
+  ArrowSmallLeftIcon,
+  ArrowSmallRightIcon,
+} from "@heroicons/react/24/solid";
+import { Zoom } from "react-slideshow-image";
+import { text } from "stream/consumers";
 
 const About = () => {
-  return (
-    <section id='about'className='w-100 bg-stone-100 border-y-4 border-stone-600'>
-        dsad
-    </section>
-  )
-}
+  const sliderTexts = [
+    {
+      title: "Cutting-Edge Chart Analysis:",
+      value: `Our platform provides in-depth chart analysis using the latest technical indicators, patterns, and algorithms. Whether you're interested in day trading, swing trading, or long-term investments, our charts offer invaluable insights to help you make informed choices.`,
+    },
+    {
+      title: "Real-Time Forex Information:",
+      value: `Stay ahead of the curve with up-to-the-minute Forex news, market updates, and economic events. Our team of experts scours the financial landscape to deliver timely and relevant information directly to you.`,
+    },
+    {
+      title: "Educational Resources:",
+      value: `We believe that knowledge is power. That's why we offer a wealth of educational resources, including tutorials, articles, and webinars, to help you sharpen your trading skills and deepen your understanding of Forex.`,
+    },
+    {
+      title: "User-Friendly Interface:",
+      value: `TopForX boasts an intuitive and user-friendly interface, making it easy for traders of all backgrounds to access critical data and conduct detailed analyses with ease.`,
+    },
+    {
+      title: "Community and Support:",
+      value: `Join our thriving community of Forex enthusiasts. Share insights, discuss strategies, and seek advice from fellow traders. Additionally, our dedicated support team is always ready to assist you with any questions or issues you may encounter.`,
+    },
+    {
+      title: "Customization:",
+      value: `Tailor your TopForX experience to your unique needs and preferences. Customize your dashboard, set alerts, and personalize your charts to suit your trading style.`,
+    },
+  ];
+  const [texts, setTexts] = useState(sliderTexts[0]);
+  const changeSlide = (side) => {
+    let arrSize = sliderTexts.length - 1;
+    let arrIndex = sliderTexts.findIndex((item) => item.title === texts.title);
+    if (side === "prev") {
+      if (arrIndex != 0) {
+        setTexts(sliderTexts[arrIndex - 1]);
+      } else {
+        setTexts(sliderTexts[arrSize]);
+      }
+    } else {
+      if (arrIndex != arrSize) {
+        setTexts(sliderTexts[arrIndex + 1]);
+      } else {
+        setTexts(sliderTexts[0]);
+      }
+    }
+  };
+  setTimeout(changeSlide,10000)
 
-export default About
+  return (
+    <div id="about" className="py-10 h-max w-full grid grid-cols-6 bg-stone-100 border-y-4 border-stone-600">
+      <div className="col-span-1 self-center justify-self-end ml-10">
+        <ArrowSmallLeftIcon
+          id="prev"
+          className="h-8 w-8 text-black cursor-pointer"
+          onClick={(e) => changeSlide("prev")}
+        />
+      </div>
+      <div className="col-span-4 text-center">
+        <h2 className="text-2xl">{texts?.title}</h2>
+        <p>{texts?.value}</p>
+      </div>
+      <div className="col-span-1 self-center mr-10">
+        <ArrowSmallRightIcon
+          className="h-8 w-8 text-black cursor-pointer"
+          onClick={(e) => changeSlide("next")}
+        />
+      </div>
+    </div>
+  );
+};
+export default About;
--- app/components/Introduce.tsx
+++ app/components/Introduce.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Chart from './chart'
+import Chart from './Chart'
 
 const Introduce = () => {
   return (
     <div className='grid grid-cols-12 gap-10  px-32 py-10 text-2xl text-stone-700'>
-    <div className="col-span-6 border-2 border-black">
+    <div className="col-span-7 border-2 border-black">
         <Chart/>
     </div>
-    <div className="col-span-6 text-end" >
+    <div className="col-span-5 text-end" >
     Welcome to TopForX, your premier destination for chart analysis and comprehensive Forex information.
     If you're looking to navigate the dynamic world of foreign exchange trading with confidence and precision,
      you've come to the right place.
